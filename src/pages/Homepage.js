6d28984f15c588a518813db6d6995d10752423a0
rachelrj <rachel@brainjoltmedia.com>
2022-03-13 17:13:19 -0600

    Update header image

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Listing from '../components/Listing';
 import BlogPost from '../components/BlogPost';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
